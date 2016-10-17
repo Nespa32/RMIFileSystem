0b063d69b025aa1cf22debda9d5c5f15a70022a7
Ricardo <nespacontrib@gmail.com>
2016-10-17 22:20:54 +0100

    Add basic FileSystem 'bookkeeping' to MetaServer

--- src/MetaServer.java
+++ src/MetaServer.java
@@ -8,19 +8,43 @@ import java.rmi.RemoteException;
 
 public class MetaServer implements MetaServerInterface {
 
+    public static void main(String args[]) {
+
+        // setup MetaServer
+        try {
+
+            MetaServer s = new MetaServer();
+            MetaServerInterface metaServer = (MetaServerInterface)UnicastRemoteObject.exportObject(s, 0);
+            Registry registry = LocateRegistry.getRegistry();
+            registry.bind("MS", metaServer);
+
+            System.out.println("MetaServer ready");
+        }
+        catch (Exception e) {
+
+            System.err.println("MetaServer exception: " + e.toString());
+            e.printStackTrace();
+        }
+    }
+
     // StorageServer ID -> Filesystem Path
-    HashMap<String, String> storageServerMap;
-    int nextStorageServer;
+    private Map<String, String> storageServerMap;
+    private FileSystemObject rootObj;
+    private int nextStorageServer;
 
     public MetaServer()
     {
+        storageServerMap = new HashMap<String, String>();
+        rootObj = new FileSystemObject(null, "/", true);
         nextStorageServer = 0;
     }
 
 
     // methods used by Client
     @Override
-    public String find(String path) {
+    public String find(String path) throws RemoteException {
+
+        // @todo
         return "find";
     }
 
@@ -51,31 +75,98 @@ public class MetaServer implements MetaServerInterface {
     }
 
     @Override
-    public void notifyItemAdd(String path) {
-        // @todo
+    public void notifyItemAdd(String path) throws RemoteException {
+
+        String[] splitPath = path.split("/");
+        if (splitPath.length == 0)
+            throw new RemoteException("Bad path <" + path + ">");
+
+        FileSystemObject existingObj = getObjectForPath(path);
+        if (existingObj != null)
+            throw new RemoteException("Item already exists");
+
+        boolean isDirectory = path.endsWith("/");
+        String name = splitPath[splitPath.length - 1];
+
+        String subPath = "";
+        for (int i = 0; i < splitPath.length - 1; ++i)
+            subPath += "/" + splitPath[i];
+
+        FileSystemObject obj = getObjectForPath(subPath);
+        if (obj == null)
+            throw new RemoteException("Couldn't find parent directory for item");
+
+        FileSystemObject child = new FileSystemObject(obj, name, isDirectory);
     }
 
     @Override
-    public void notifyItemDelete(String path) {
-        // @todo
+    public void notifyItemDelete(String path) throws RemoteException {
+
+        FileSystemObject obj = getObjectForPath(path);
+        if (obj == null)
+            throw new RemoteException("Item not found, can't delete");
+        else if (obj == rootObj)
+            throw new RemoteException("Did you really just try to delete root?");
+
+        obj.getParent().removeChild(obj);
     }
 
-    public static void main(String args[]) {
 
-        // setup MetaServer
-        try {
+    private FileSystemObject getObjectForPath(String path) {
 
-            MetaServer s = new MetaServer();
-            MetaServerInterface metaServer = (MetaServerInterface)UnicastRemoteObject.exportObject(s, 0);
-            Registry registry = LocateRegistry.getRegistry();
-            registry.bind("MS", metaServer);
+        String[] splitPath = path.split("/");
 
-            System.out.println("MetaServer ready");
+        FileSystemObject obj = rootObj;
+        for (String s : splitPath)
+        {
+            obj = obj.getChild(s);
+            if (obj == null)
+                break;
         }
-        catch (Exception e) {
 
-            System.err.println("MetaServer: " + e.toString());
-            e.printStackTrace();
-        }
+        return obj;
     }
- }
+}
+
+class FileSystemObject implements Comparable<FileSystemObject>
+{
+    private FileSystemObject parent;
+    private String name;
+    private boolean isDirectory;
+    private Map<String, FileSystemObject> children;
+
+    @Override
+    public int compareTo(FileSystemObject other) {
+        return this.name.compareTo(other.name);
+    }
+
+    public FileSystemObject(FileSystemObject parent, String name, boolean isDirectory) {
+
+        this.parent = parent;
+        this.name = name;
+        this.isDirectory = isDirectory;
+        this.children = new TreeMap<String, FileSystemObject>();
+    }
+
+    public void addChild(FileSystemObject child) {
+        children.put(child.getName(), child);
+    }
+
+    public void removeChild(FileSystemObject child) {
+        children.remove(child.getName());
+    }
+
+    public FileSystemObject getChild(String name) {
+
+        FileSystemObject obj = children.get(name);
+        return obj;
+    }
+
+    public FileSystemObject getParent() {
+        return parent;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
