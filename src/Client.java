c2d5f22f6d9d105933b6943289ce7a7303f4dfd5
Andre <a_cascais@hotmail.com>
2016-10-17 18:56:03 +0100

    Tabs are now 4 spaces git status Also some error management in command parsing added to Client.java besides this i just want to remind you that ricardo hates long messages

--- src/Client.java
+++ src/Client.java
@@ -1,19 +1,114 @@
 import java.rmi.registry.Registry;
 import java.rmi.registry.LocateRegistry;
+import java.util.Scanner;
 
 public class Client {
 
+    private final Registry registry;
+    private final MetaServerInterface metaServer;
+    private String myPwd;
+
+    public Client(Registry _registry, MetaServerInterface _metaServer) {
+
+    registry = _registry;
+    metaServer = _metaServer;
+    myPwd = "/";
+    }
+    // Command Functions
+
+    public void pwd(String[] cmd) {
+
+    if (cmd.length != 1) {
+        System.err.println("pwn doesn't have any arguments");
+        return ;
+    }
+    System.out.println(myPwd);
+    }
+
+    public void ls(String[] cmd) {
+
+    if (cmd.length != 1) {
+        System.err.println("ls doesn't have any arguments");
+        return ;
+    }
+    // String[] l = metaServer.list(myPwd);
+        String[] s = { "a.out", "temp/" };
+    // Iterates trough the list and prints the items
+    for (String i: s) {
+        System.out.println(i + " ");
+    }
+    // ...
+    }
+    
+    public void cd(String[] cmd) {
+
+    if (cmd.length != 2) {
+        System.err.println("Expected format cd dir");
+        return ;
+    }
+    }
+
+    public void mv(String[] cmd) {
+
+    if (cmd.length != 3) {
+        System.err.println("Expected format mv file1 file2");
+        return ;
+    }
+    }
+
+    public void open(String[] cmd) {
+
+    if (cmd.length != 2) {
+        System.err.println("Expected format open file");
+        return ;
+    }
+    }
+    
     public static void main(String args[]) {
 
-	try {
-	    
-	    Registry registry = LocateRegistry.getRegistry();
-	    MetaServerInterface stub = (MetaServerInterface)registry.lookup("MS");
-	    String response = stub.Find();
-	    System.out.println("Response: " + response);
-	}
-	catch (Exception e) {
-	    // @todo
-	}
+    Registry registry = null;
+    MetaServerInterface metaServer = null;
+    
+    try {
+        // registry = LocateRegistry.getRegistry();
+        // metaServer = (MetaServerInterface)registry.lookup("MS");
+
+        Client client = new Client(registry, metaServer);
+        client.run();
+    }
+    catch (Exception e) {
+        // @todo
+    }
+    
+    }
+
+    void run() {
+    // @todo: commands, etc
+    Scanner scanner = new Scanner(System.in);
+    while(true) {
+        String[] cmd = scanner.nextLine().split(" ");
+        switch(cmd[0]) {
+        case "": // empty string case, user pressed ENTER, simply ignore
+            break;
+            case "pwd":
+            pwd(cmd);
+            break;
+            case "ls":
+            ls(cmd);
+            break;
+            case "cd":
+            cd(cmd);
+            break;
+            case "mv":
+            mv(cmd);
+            break;
+            case "open":
+            open(cmd);
+            break;
+            default:
+            System.err.println("Invalid command " + cmd[0]);
+            break;
+        }
+    }
     }
 }
