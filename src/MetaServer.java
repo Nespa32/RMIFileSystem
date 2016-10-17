fe8b34c0a4fcb58441992bfb14ab3589d241398c
Ricardo <nespacontrib@gmail.com>
2016-10-17 18:08:38 +0100

    MetaServer compile fixes, minor work on methods

--- src/MetaServer.java
+++ src/MetaServer.java
@@ -1,60 +1,81 @@
+
+import java.util.*;
 import java.rmi.registry.Registry;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.server.UnicastRemoteObject;
-
+import java.rmi.Remote;
+import java.rmi.RemoteException;
 
 public class MetaServer implements MetaServerInterface {
 
-    
-    
-    public MetaServer() { }
+    // StorageServer ID -> Filesystem Path
+    HashMap<String, String> storageServerMap;
+    int nextStorageServer;
 
-    // Client Methods
-    
+    public MetaServer()
+    {
+        nextStorageServer = 0;
+    }
+
+
+    // methods used by Client
     @Override
-    public String find() {
-	return "fInD";
+    public String find(String path) {
+	    return "find";
     }
 
     @Override
-    public List<String>  list() {
+    public List<String> list(String path) {
+        // @todo
+        List<String> l = null;
+        return l;
     }
 
-    // Storage Server Methods
-
+    // methods used by StorageServer
     @Override
     public String subscribe() {
+
+        String s = String.format("SS_%d", nextStorageServer);
+        nextStorageServer += 1;
+        // @todo: how to distribute (?)
+        storageServerMap.put(s, "/");
+
+        return s;
     }
 
     @Override
-    public void unsubscribe() {
+    public void unsubscribe(String s) throws RemoteException {
+        String path = storageServerMap.remove(s);
+        if (path == null)
+            throw new RemoteException("StorageServer " + s + " not found!");
     }
 
     @Override
     public void notifyItemAdd(String path) {
+        // @todo
     }
 
     @Override
     public void notifyItemDelete(String path) {
+        // @todo
     }
-    
+
     public static void main(String args[]) {
 
-	// setup MetaServer
-	try {
-	    
-	    MetaServer s = new MetaServer();
-	    MetaServerInterface stub = (MetaServerInterface)UnicastRemoteObject.exportObject(s, 0);
-	    Registry registry = LocateRegistry.getRegistry();
-	    registry.bind("MS", stub);
-	    System.err.println("MetaServer ready");
-	}
-	catch (Exception e) {
-	    
-	    System.err.println("MetaServer: " + e.toString());
-	    e.printStackTrace();
-	}
+	    // setup MetaServer
+	    try {
 
-	// test availability
+	        MetaServer s = new MetaServer();
+    	    MetaServerInterface metaServer = (MetaServerInterface)UnicastRemoteObject.exportObject(s, 0);
+	        Registry registry = LocateRegistry.getRegistry();
+	        registry.bind("MS", metaServer);
+
+    	    System.out.println("MetaServer ready");
+	    }
+	    catch (Exception e) {
+
+	        System.err.println("MetaServer: " + e.toString());
+	        e.printStackTrace();
+	    }
     }
  }
