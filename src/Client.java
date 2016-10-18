78b9ae530ac94c682fcf4d1174855a0dd5eadff0
Andre <a_cascais@hotmail.com>
2016-10-18 16:14:32 +0100

    Fix indentation in Client.java

--- src/Client.java
+++ src/Client.java
@@ -7,108 +7,108 @@ public class Client {
     private final Registry registry;
     private final MetaServerInterface metaServer;
     private String myPwd;
-
+    
     public Client(Registry _registry, MetaServerInterface _metaServer) {
-
-    registry = _registry;
-    metaServer = _metaServer;
-    myPwd = "/";
+        
+        registry = _registry;
+        metaServer = _metaServer;
+        myPwd = "/";
     }
     // Command Functions
 
     public void pwd(String[] cmd) {
-
-    if (cmd.length != 1) {
-        System.err.println("pwn doesn't have any arguments");
-        return ;
+        
+        if (cmd.length != 1) {
+            System.err.println("pwn doesn't have any arguments");
+            return ;
+        }
+        System.out.println(myPwd);
     }
-    System.out.println(myPwd);
-    }
-
+    
     public void ls(String[] cmd) {
-
-    if (cmd.length != 1) {
-        System.err.println("ls doesn't have any arguments");
-        return ;
-    }
-    // String[] l = metaServer.list(myPwd);
+        
+        if (cmd.length != 1) {
+            System.err.println("ls doesn't have any arguments");
+            return ;
+        }
+        // String[] l = metaServer.list(myPwd);
         String[] s = { "a.out", "temp/" };
-    // Iterates trough the list and prints the items
-    for (String i: s) {
-        System.out.println(i + " ");
-    }
-    // ...
+        // Iterates trough the list and prints the items
+        for (String i: s) {
+            System.out.println(i + " ");
+        }
+        // ...
     }
     
     public void cd(String[] cmd) {
 
-    if (cmd.length != 2) {
-        System.err.println("Expected format cd dir");
-        return ;
-    }
+        if (cmd.length != 2) {
+            System.err.println("Expected format cd dir");
+            return ;
+        }
     }
 
     public void mv(String[] cmd) {
-
-    if (cmd.length != 3) {
-        System.err.println("Expected format mv file1 file2");
-        return ;
-    }
+    
+        if (cmd.length != 3) {
+            System.err.println("Expected format mv file1 file2");
+            return ;
+        }
     }
 
     public void open(String[] cmd) {
 
-    if (cmd.length != 2) {
-        System.err.println("Expected format open file");
-        return ;
-    }
+        if (cmd.length != 2) {
+            System.err.println("Expected format open file");
+            return ;
+        }
     }
     
     public static void main(String args[]) {
 
-    Registry registry = null;
-    MetaServerInterface metaServer = null;
+        Registry registry = null;
+        MetaServerInterface metaServer = null;
     
-    try {
-        // registry = LocateRegistry.getRegistry();
-        // metaServer = (MetaServerInterface)registry.lookup("MS");
+        try {
+            // registry = LocateRegistry.getRegistry();
+            // metaServer = (MetaServerInterface)registry.lookup("MS");
 
-        Client client = new Client(registry, metaServer);
-        client.run();
-    }
-    catch (Exception e) {
-        // @todo
-    }
+            Client client = new Client(registry, metaServer);
+            client.run();
+        }
+        catch (Exception e) {
+            // @todo
+        }
     
     }
 
     void run() {
-    // @todo: commands, etc
-    Scanner scanner = new Scanner(System.in);
-    while(true) {
-        String[] cmd = scanner.nextLine().split(" ");
-        switch(cmd[0]) {
-        case "": // empty string case, user pressed ENTER, simply ignore
-            break;
+        // @todo: commands, etc
+        Scanner scanner = new Scanner(System.in);
+        while(true) {
+            String[] cmd = scanner.nextLine().split(" ");
+            switch(cmd[0]) {
+            case "": // empty string case, user pressed ENTER, simply ignore
+                break;
             case "pwd":
-            pwd(cmd);
-            break;
+                pwd(cmd);
+                break;
             case "ls":
-            ls(cmd);
-            break;
+                ls(cmd);
+                break;
             case "cd":
-            cd(cmd);
-            break;
+                cd(cmd);
+                break;
             case "mv":
-            mv(cmd);
-            break;
+                mv(cmd);
+                break;
             case "open":
-            open(cmd);
-            break;
+                open(cmd);
+                break;
             default:
-            System.err.println("Invalid command " + cmd[0]);
-            break;
+                System.err.println("Invalid command " + cmd[0]);
+                break;
+            }
         }
-    }
     }
 }
