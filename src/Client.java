7baf85f81deba77b5e137b02fcdbef7d45cb6f5a
Andre <a_cascais@hotmail.com>
2016-10-19 12:58:31 +0100

    Changes to cd and ls based on the find/list methods from MS

--- src/Client.java
+++ src/Client.java
@@ -1,3 +1,4 @@
+import java.util.*;
 import java.rmi.registry.Registry;
 import java.rmi.registry.LocateRegistry;
 import java.util.Scanner;
@@ -7,9 +8,9 @@ public class Client {
     private final Registry registry;
     private final MetaServerInterface metaServer;
     private String myPwd;
-    
+
     public Client(Registry _registry, MetaServerInterface _metaServer) {
-        
+
         registry = _registry;
         metaServer = _metaServer;
         myPwd = "/";
@@ -17,43 +18,95 @@ public class Client {
     // Command Functions
 
     public void pwd(String[] cmd) {
-        
+
         if (cmd.length != 1) {
-            System.err.println("pwn doesn't have any arguments");
+            System.err.println("pwd doesn't have any arguments");
             return ;
         }
         System.out.println(myPwd);
     }
-    
+
     public void ls(String[] cmd) {
-        
+
         if (cmd.length != 1) {
             System.err.println("ls doesn't have any arguments");
             return ;
         }
-        // String[] l = metaServer.list(myPwd);
-        String[] s = { "a.out", "temp/" };
-        // Iterates trough the list and prints the items
-        for (String i: s) {
-            System.out.println(i + " ");
+        List<String> l;
+        try {
+            l = metaServer.list(myPwd);
         }
-        // ...
+        catch (Exception e){
+
+            System.err.println(e);
+            return;
+        }
+        // Iterates trough the list and prints the items
+        for (String i : l)
+            System.out.println(i + " ");
     }
-    
+
     public void cd(String[] cmd) {
 
         if (cmd.length != 2) {
             System.err.println("Expected format cd dir");
             return ;
         }
+        // Checking if the dir path is relative or absolute
+        String tempPwd = (cmd[1].charAt(0) == '/' ? "/" : myPwd);
+        String[] pathSplited = cmd[1].split("/");
+        String absPath = buildPath(tempPwd, pathSplited);
+
+        if (!checkPath(absPath)) {
+           System.err.println("Path not valid");
+           return;
+        }
+        else
+            myPwd = absPath;
+    }
+    public String buildPath(String tempPwd, String []remainingPwd){
+
+        int length = remainingPwd.length;
+        // Path creation ended
+        if (length == 0)
+            return tempPwd;
+
+        String nextDir = remainingPwd[0];
+        String []nextPwd = Arrays.copyOfRange(remainingPwd, 1, length);
+
+        // Deal with changing to ..
+        if (nextDir.equals("..") && !tempPwd.equals("/")) {
+            // Remove the last /
+            String newString = tempPwd.substring(0, tempPwd.lastIndexOf('/'));
+            // Remove the last dir
+            return buildPath(newString.substring(0, newString.lastIndexOf('/')) + "/", nextPwd);
+        }
+        // When we don't change to any dir
+        else if (nextDir.equals(".") || nextDir.equals(".."))
+            return buildPath(tempPwd, nextPwd);
+        else
+            return buildPath(tempPwd + nextDir + "/", nextPwd);
+    }
+
+    public boolean checkPath(String path) {
+        try {
+            metaServer.find(path);
+            return true;
+        }
+        catch (Exception e){
+            System.out.println(e);
+            return false;
+        }
     }
 
     public void mv(String[] cmd) {
-    
+
         if (cmd.length != 3) {
             System.err.println("Expected format mv file1 file2");
             return ;
         }
+
+        String[] s = { "a.out", "temp/" };
     }
 
     public void open(String[] cmd) {
@@ -63,15 +116,15 @@ public class Client {
             return ;
         }
     }
-    
+
     public static void main(String args[]) {
 
         Registry registry = null;
         MetaServerInterface metaServer = null;
-    
+
         try {
-            // registry = LocateRegistry.getRegistry();
-            // metaServer = (MetaServerInterface)registry.lookup("MS");
+            registry = LocateRegistry.getRegistry();
+            metaServer = (MetaServerInterface)registry.lookup("MS");
 
             Client client = new Client(registry, metaServer);
             client.run();
@@ -79,7 +132,7 @@ public class Client {
         catch (Exception e) {
             // @todo
         }
-    
+
     }
 
     void run() {
